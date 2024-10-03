0247ad70686ecfe101d14db96e55eafcb3a07d53
marcodamecourt <depontonda@cy-tech.fr>
2024-10-03 11:52:28 +0200

    fast_api_backend

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-# app.py
-from flask import Flask, render_template
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import pandas as pd
 
-app = Flask(__name__)
+app = FastAPI()
 
-# Load the dataset
-df = pd.read_csv('Spotify_Song_Attributes.csv')
+# Configuration des templates
+templates = Jinja2Templates(directory="templates")
 
-@app.route('/')
-def home():
-    # Convert DataFrame to HTML
-    table = df.to_html(classes='table table-striped')
-    return render_template('index.html', table=table)
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, Spotify Song Attributes!"}
 
-if __name__ == '__main__':
-    app.run(debug=False)
+@app.get("/data", response_class=HTMLResponse)
+async def get_data(request: Request):
+    # Charger le fichier CSV dans un DataFrame
+    df = pd.read_csv('Spotify_Song_Attributes.csv')  # Ajuster le chemin si nécessaire
+
+    # Convertir le DataFrame en HTML
+    table_html = df.to_html(classes='table table-striped')
+
+    # Rendre le template avec la table
+    return templates.TemplateResponse("index.html", {"request": request, "table": table_html})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
+
